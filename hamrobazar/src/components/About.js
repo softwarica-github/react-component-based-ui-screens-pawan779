70f98dc0c086842801f474cd233c1dd423607cb0
pawan779 <pawan.dharel77@gmail.com>
2020-01-23 22:03:53 +0545

    about and contact page added

---
+++ hamrobazar/src/components/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Container } from 'reactstrap'
+
+class About extends Component {
+    render() {
+        return (
+        <Container>
+                <h1>About page</h1>
+
+                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam doloribus magni veniam, recusandae voluptate ullam explicabo amet soluta excepturi laudantium quo magnam pariatur alias sequi nulla similique deserunt, aut fugit.
+                Quos perspiciatis, quidem vel adipisci maiores veniam? Quos aspernatur aliquid est quasi exercitationem maxime sunt dicta? At accusantium praesentium exercitationem nisi porro quo quae odio dignissimos commodi? Saepe, inventore molestiae!
+                Blanditiis quas ratione suscipit ea amet, eveniet provident unde corporis fuga aperiam sunt quod numquam, cumque aliquid et architecto. Quidem quasi, eligendi aspernatur adipisci sed magnam eius tempora porro atque!
+                Dolor quam quod fugit, labore beatae quia et itaque corrupti, pariatur sequi minus dolore maxime, commodi deserunt dicta animi consequuntur fuga vitae laboriosam id autem! Quaerat vel molestiae dignissimos quibusdam?
+                Harum labore totam distinctio enim perferendis debitis. Tempora perferendis porro dignissimos. Perspiciatis corrupti maiores magni iste facere, quis ipsam quod ratione nihil iure perferendis, accusantium excepturi corporis ad veniam. Reprehenderit.</span>
+                </Container>
+        )
+    }
+}
+
+export default About
